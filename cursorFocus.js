7fc3e632ae4550e1db90acf45dc65ed0dc823a4f
Akashonbeats <akash.s1204@gmail.com>
2025-05-11 01:30:58 +0530

    Cursor now auto places itself after the last char

--- cursorFocus.js
+++ cursorFocus.js
@@ -1,7 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   const notepad = document.querySelector(".notepad");
+
+  function placeCaretAtEnd(element) {
+    if (!element.lastChild) {
+      element.appendChild(document.createTextNode(''));
+    }
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(element);
+    range.collapse(false);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+
   notepad.focus();
+  placeCaretAtEnd(notepad);
+
   window.addEventListener("pageshow", () => {
     notepad.focus();
+    placeCaretAtEnd(notepad);
   });
 });
