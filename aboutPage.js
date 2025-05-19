e32c8efafb269a978fec00682bec3ac3e66056d2
Akash Sampath <akash.s1204@gmail.com>
2025-05-20 00:23:49 +0530

    Added a about page that triggers with a checkbox

---
+++ aboutPage.js
@@ -0,0 +1,23 @@
+document.getElementById("infoCheckBox").addEventListener("change", function() {
+    const infoBox = document.querySelector(".app");
+    const formatBar = document.querySelector(".format-bar");
+    const aboutPage = document.querySelector(".about-page");
+
+
+    if (this.checked) {
+        // Function to execute when checkbox is checked
+        const enabled = () => {
+            console.log("Checkbox is checked");
+            infoBox.style.opacity = "0";
+            formatBar.style.opacity = "0";
+            aboutPage.style.display = "block";
+        };
+        enabled();
+    } else {
+        // Function to execute when checkbox is unchecked
+        console.log("Checkbox is unchecked");
+        infoBox.style.opacity = "1";
+        formatBar.style.opacity = "1";
+        aboutPage.style.display = "none";
+    }
+});
