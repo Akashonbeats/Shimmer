b902057d680ce52dc3228a6607fe751072c71114
Akash Sampath <akash.s1204@gmail.com>
2025-05-19 20:21:59 +0530

    Added scripts to handle the formatting of bius bar

---
+++ formatBarHandler.js
@@ -0,0 +1,15 @@
+function formatBold() {
+  document.execCommand('bold', false, null);
+}
+
+function formatItalic() {
+  document.execCommand('italic', false, null);
+}
+
+function formatUnderline() {
+  document.execCommand('underline', false, null);
+}
+
+function formatStrike() {
+  document.execCommand('strikeThrough', false, null);
+}
